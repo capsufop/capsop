86a900b0db479cf9d2c28adbbff7c5a1a7b036ea
Daniel M Nobre <damnobre@gmail.com>
2014-04-26 13:07:42 -0300

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Capsop::Application.config.secret_key_base = 'b0ec426b70d36445c80e6856eddce1e5942813a52232624f73438899371507afe0b421f1e9b90ad5207313dc56e3091c7c200168c6ba38fbfe5402d5a14da277'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Capsop::Application.config.session_store :cookie_store, key: '_capsop_session'
